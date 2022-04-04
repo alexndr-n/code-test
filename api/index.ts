ca330271fb93804225cbc4918c48a8e79f218399
Alejandro Nieto <ale.nieto97@gmail.com>
2022-04-04 08:34:38 -0300

    alejandro code

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,53 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { initialBalance, initialItems } from './data';
+import { initialBalance, initialItems, Item } from './data';
+import cors from 'cors';
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors())
+
+const currentItems = [...initialItems]
+let currentBalance = (+initialBalance)
 
 app.get('/', async (req, res) => {
   res.send('ok');
 });
 
 app.get('/items', async (req, res) => {
-  // const data = req.body;
-  throw new Error('Not implemented');
-  // res.send(initialItems);
+  try {
+    res.send(currentItems).end()
+  } catch (error) {
+    console.error(error)
+    res.status(500).end
+  }
 });
+
+app.get('/balance', async (req, res) => {
+  try {
+    res.send(currentBalance).end()
+  } catch (error) {
+    console.error(error)
+    res.status(500).end
+  }
+});
+
+app.post('/purchase', async (req, res) => {
+  try {
+    const { itemId, quantity } = req.body;
+    const item: Item | undefined = currentItems.find(item => item.id === itemId)
+    if(!item) return res.status(500).end()
+    if(item.inventory < quantity) return res.status(402).send('Not enough inventory');
+    if(item.price * quantity > currentBalance) return res.status(402).send('Not enough balance');
+    item.inventory -= quantity;
+    currentBalance -= quantity * item.price;
+    res.status(200).send({balance: currentBalance}).end()
+  } catch (error) {
+    console.error(error)
+    res.status(500).end
+  }
+});
+
 app.listen(8000, () => {
   console.log('🚀 Server started 🚀');
 });
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './index.module.scss';
+import axios from 'axios';
+
+const apiUrl = 'http://localhost:8000'
 
 const Index = () => {
-  const buy = () => {
-    throw new Error('Not implemented');
-    // fetch('http://localhost:8000/buy', {
-    // }).then((response) => response.json());
+
+  const [items, setItems] = useState([])
+
+  axios.get(apiUrl + '/items')
+  .then(data => {
+    console.log(data.data)
+    setItems(data.data)
+  })
+  .catch(error => console.error(error))
+
+  const buy = (id) => {
+    axios.post(apiUrl + '/purchase', {itemId: id, quantity: 1})
+    axios.get(apiUrl + '/items')
+    .then(data => {
+      console.log(data.data)
+      setItems(data.data)
+    })
+    .catch(error => console.error(error))
   };
 
   return (
     <main className={styles.main}>
       <h1>Create, Inc. Store</h1>
-      {/** @TODO: Not implemented */}
+      <div>
+        {items.map(item => (
+          <div>
+            <p>{item.id}</p>
+            <p>{item.name}</p>
+            <p>{item.price}</p>
+            <p>{item.inventory}</p>
+            <button onClick={() => buy(item.id)}>Buy</button>
+          </div>
+        ))}
+      </div>
     </main>
   );
 };
